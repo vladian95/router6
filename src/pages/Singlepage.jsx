36b3b10f9113638a6a99598e09ec2bba25be7307
vladian95 <kvankvan1995@gmail.com>
2023-05-11 13:17:55 +0300

    start useNavigate

--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,8 +1,9 @@
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 const Singlepage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
