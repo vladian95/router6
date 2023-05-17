75cba9e14e1b509a413e25081f2523864be644cb
vladian95 <kvankvan1995@gmail.com>
2023-05-17 19:48:49 +0300

    nested route

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
 const About = () => {
   return (
     <div>
       <h1>About us</h1>
       <p>This is demo website about React</p>
+      <ul>
+        <li>
+          <Link to="contacts">Our contacts</Link>
+        </li>
+        <li>
+          <Link to="team">Our team</Link>
+        </li>
+      </ul>
+      {/* <Routes>
+        <Route path="contacts" element={<p>Our contact</p>} />
+        <Route path="team" element={<p>Our team</p>} />
+      </Routes> */}
+      <Outlet />
     </div>
   );
 };
